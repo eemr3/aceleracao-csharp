79db4c5a245fed5c517abbab3cbae1412f8515e5
Emerson Moreira <eemr3@yahoo.com.br>
2024-11-19 11:42:03 -0300

    datas iniciando o estudo

---
+++ secao_02/aula_04/Datas/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+  public static void Main(string[] ags)
+  {
+    GetDateNow();
+    string momento = GetTimeNow();
+    Console.WriteLine(momento);
+
+    GeDateDay();
+  }
+
+  public static void GetDateNow()
+  {
+    var date = new DateTime(2024, 11, 19, 11, 36, 0);
+    var dateOnly = date.Date;
+    Console.WriteLine(dateOnly.ToString());
+  }
+
+  public static string GetTimeNow()
+  {
+    return "O momento de tempo atual é " + DateTime.Now;
+  }
+
+  public static void GeDateDay()
+  {
+    var date = new DateTime(2024, 11, 19, 11, 39, 0);
+    var dayOnly = date.Day;
+    Console.WriteLine(dayOnly.ToString());
+  }
+}
