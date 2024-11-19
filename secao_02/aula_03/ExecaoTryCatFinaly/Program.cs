1740a128a6f835668af1f80460776521420a67a6
Emerson Moreira <eemr3@yahoo.com.br>
2024-11-19 10:38:35 -0300

    Execcão clausula Try-Catch-Finaly

---
+++ secao_02/aula_03/ExecaoTryCatFinaly/Program.cs
@@ -0,0 +1,53 @@
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    try
+    {
+      InitialGreeting();
+      int year = Convert.ToInt32(Console.ReadLine());
+      var age = CalculateAgeByYear(year);
+
+      Console.WriteLine("A sua idade é: " + age + " anos!");
+      ValidateComingOfAge(age);
+
+    }
+    catch (AccessViolationException err)
+    {
+      Console.WriteLine("Error no acesso: " + err.Message);
+
+    }
+    catch (Exception err)
+    {
+      Console.WriteLine("Error: " + err.Message);
+    }
+    finally
+    {
+      Console.WriteLine("Software finalizado!");
+    }
+
+  }
+
+  public static void InitialGreeting()
+  {
+    Console.WriteLine("Controle de acesso!");
+    Console.WriteLine("Digite o ano de nascimento: ");
+  }
+
+  public static int CalculateAgeByYear(int year)
+  {
+    int age = DateTime.Now.Year - year;
+
+    return age;
+  }
+
+  public static void ValidateComingOfAge(int age)
+  {
+
+    if (age < 18)
+    {
+      throw new AccessViolationException("Mernor de idade!");
+    }
+
+  }
+}
