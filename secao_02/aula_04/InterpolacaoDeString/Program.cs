3e807d9e4422a68559452190541c851ca60add76
Emerson Moreira <eemr3@yahoo.com.br>
2024-11-19 11:21:52 -0300

    interpolando strings no C#

---
+++ secao_02/aula_04/InterpolacaoDeString/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    string name = getName();
+
+    Console.WriteLine($"O seu nome é {name}");
+  }
+
+  public static string getName()
+  {
+    Console.WriteLine("Qual o seu nome? ");
+    string? name = Console.ReadLine();
+
+    return name ?? "Nome desconhecido";
+  }
+}
