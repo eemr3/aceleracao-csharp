8e0194c387f0ba82fe5fa635e20ffaaa9c69d638
Emerson Moreira <eemr3@yahoo.com.br>
2024-11-19 09:44:23 -0300

    função sem retorno em C#

---
+++ secao_02/aula_03/FuncaoSemRetorno/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    FuncaoSemRetorno();
+  }
+
+  public static void FuncaoSemRetorno()
+  {
+    Console.WriteLine("Sou uma função que não retorna nada, e sim executa algo. Como o log do console.");
+  }
+}
