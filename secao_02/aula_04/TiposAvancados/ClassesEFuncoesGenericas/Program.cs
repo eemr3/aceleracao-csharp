8d9fa53e8dfb2ffbbef098cbf2077ef4eb9b65e0
Emerson Moreira <eemr3@yahoo.com.br>
2024-11-20 11:22:57 -0300

    classes e funções genericas

---
+++ secao_02/aula_04/TiposAvancados/ClassesEFuncoesGenericas/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+  public static void Main(string[] args)
+  {
+    string x = "Trybe";
+    string y = "C#";
+    Console.WriteLine($"Antes da troca - x = {x} - y = {y}");
+
+    TrocarVariaveis(ref x, ref y);
+
+    Console.WriteLine($"Depois da troca - x = {x} - y = {y}");
+
+    int n1 = 4;
+    int n2 = 7;
+    Console.WriteLine($"Antes da troca - n1 = {n1} - n2 = {n2}");
+
+    TrocarVariaveis(ref n1, ref n2);
+
+    Console.WriteLine($"Depois da troca - n1 = {n1} - n2 = {n2}");
+  }
+
+  public static void TrocarVariaveis<T>(ref T variavelA, ref T variavelB)
+  {
+    T tempVariavel = variavelA;
+    variavelA = variavelB;
+    variavelB = tempVariavel;
+
+  }
+}
