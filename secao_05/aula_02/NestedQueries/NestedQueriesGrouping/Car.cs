64434a1de6565ad99e2836b3b1ddbdd7bc2f97f7
Emerson Moreira <eemr3@yahoo.com.br>
2024-12-05 11:12:29 -0300

    aula 02 da secao 05 - Consultas Aninhadas - Consultas aninhadas em Agrupamento

---
+++ secao_05/aula_02/NestedQueries/NestedQueriesGrouping/Car.cs
@@ -0,0 +1,8 @@
+namespace NestedQueriesGrouping;
+
+public class Car
+{
+  public string? Brand { get; set; }
+  public string? Model { get; set; }
+  public double Price { get; set; }
+}
---
+++ secao_05/aula_02/NestedQueries/NestedQueriesGrouping/Category.cs
@@ -0,0 +1,7 @@
+namespace NestedQueriesGrouping;
+
+public class Category
+{
+  public string? Name;
+  public List<Car>? Cars;
+}
