9b7f4060f16ab99675c8c909146957ee4b9aee30
Emerson Moreira <eemr3@yahoo.com.br>
2024-12-14 12:09:21 -0300

    add configuration context and file "appsettings.Development.json"

--- secao_07/aula_02/TrybeStore/Program.cs
+++ secao_07/aula_02/TrybeStore/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TrybeStore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
