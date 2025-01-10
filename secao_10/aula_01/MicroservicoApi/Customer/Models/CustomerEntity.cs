79e3caccb754f3ba4c8f30db8d72a710794b1468
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-10 12:47:41 -0300

    create model CustomerEntity

---
+++ secao_10/aula_01/MicroservicoApi/Customer/Models/CustomerEntity.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Customer.Models;
+
+public class CustomerEntity
+{
+  public int CustomerId { get; set; }
+  [Required]
+  public string Name { get; set; } = null!;
+  [Required]
+  public string Email { get; set; } = null!;
+
+}
