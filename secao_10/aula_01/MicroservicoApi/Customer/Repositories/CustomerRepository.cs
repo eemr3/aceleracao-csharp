674ad09d03425cc4e82beb15dd8a71617a1f2b47
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-10 15:47:42 -0300

    refactor repository Customer add methos GetCustomerByEmail

--- secao_10/aula_01/MicroservicoApi/Customer/Repositories/CustomerRepository.cs
+++ secao_10/aula_01/MicroservicoApi/Customer/Repositories/CustomerRepository.cs
@@ -15,28 +15,29 @@ public class CustomerRepository : ICustomerRepository
     _context = context;
   }
 
-  public async Task<CustomerEntity> AddCustomer(CustomerDtoRequest customerDto)
+  public async Task<CustomerEntity> AddCustomer(CustomerEntity customer)
   {
-    var customer = await _context.Customers.FirstOrDefaultAsync(cust => cust.Email.Equals(customerDto.Email));
-
-    if (customer is not null) throw new ConflictException($"O cliente com o email {customerDto.Email} já esta cadastrado.");
-
-    var customerAdd = _context.Customers.Add(new CustomerEntity
-    {
-      Name = customerDto.Name,
-      Email = customerDto.Email
-    });
+    var customerAdd = _context.Customers.Add(customer);
 
     await _context.SaveChangesAsync();
 
     return customerAdd.Entity;
   }
 
-  public async Task<CustomerEntity> GetCustomerById(int id)
+  public async Task<CustomerEntity?> GetCustomerById(int id)
   {
     var customer = await _context.Customers.FindAsync(id);
 
-    if (customer is null) throw new KeyNotFoundException($"O cliente com ID {id} não foi encontrado.");
+    if (customer is null) return null;
+
+    return customer;
+  }
+
+  public async Task<CustomerEntity?> GetCustomerByEmail(string email)
+  {
+    var customer = await _context.Customers.FirstOrDefaultAsync(cust => cust.Email.Equals(email));
+
+    if (customer is null) return null;
 
     return customer;
   }
