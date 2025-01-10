3a3f62e93befc448334ade509dcf65dae0aa8f86
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-10 14:34:50 -0300

    create custom exception filter

---
+++ secao_10/aula_01/MicroservicoApi/Order/Filters/CustomExceptionsFilter.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Order.Filters;
+
+public class CustomExceptionsFilter : ExceptionFilterAttribute
+{
+  private readonly ILogger<CustomExceptionsFilter> _logger;
+
+  public CustomExceptionsFilter(ILogger<CustomExceptionsFilter> logger)
+  {
+    _logger = logger;
+  }
+
+  public override void OnException(ExceptionContext context)
+  {
+    if (context.Exception is KeyNotFoundException notFound)
+    {
+      var response = new
+      {
+        StatusCode = 404,
+        Error = "NotFound",
+        Message = notFound.Message,
+        Timestamp = DateTime.UtcNow,
+      };
+
+      context.Result = new NotFoundObjectResult(response)
+      {
+        StatusCode = 404
+      };
+      context.ExceptionHandled = true;
+    }
+    else
+    {
+      var exception = context.Exception;
+      var request = context.HttpContext.Request;
+
+      _logger.LogError(exception,
+          "Unhandled Exception occurred.\n" +
+          "Message: {Message}\n" +
+          "Type: {Type}\n" +
+          "Stack Trace: {StackTrace}\n" +
+          "Timestamp: {Timestamp}\n" +
+          "Request Path: {RequestPath}\n" +
+          "HTTP Method: {HttpMethod}\n" +
+          "Query String: {QueryString}\n",
+          exception.Message,
+          exception.GetType().FullName,
+          exception.StackTrace,
+          DateTime.UtcNow,
+          request.Path,
+          request.Method,
+          request.QueryString
+      );
+
+      context.Result = new ObjectResult(context.Exception.Message)
+      {
+        StatusCode = 500
+      };
+      context.ExceptionHandled = true;
+    }
+  }
+}
--- secao_10/aula_01/MicroservicoApi/Order/Program.cs
+++ secao_10/aula_01/MicroservicoApi/Order/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Order.Data;
+using Order.Filters;
 using Order.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<CustomExceptionsFilter>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
