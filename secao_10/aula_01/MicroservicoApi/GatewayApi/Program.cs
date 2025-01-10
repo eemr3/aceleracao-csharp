192b700d09dee5948432b5d9bcc692766f1f4a22
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-10 16:24:50 -0300

    add configuration Ocelot in projetc, Program.cs

--- secao_10/aula_01/MicroservicoApi/GatewayApi/Program.cs
+++ secao_10/aula_01/MicroservicoApi/GatewayApi/Program.cs
@@ -1,6 +1,17 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//adicionando o arquivo ocelot.json ao projeto
+builder.Configuration.AddJsonFile("ocelot.json");
+
+//adicionando o conteúdo do arquivo às configurações do Ocelot
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//espera até a inicialização do Ocelot
+await app.UseOcelot();
 
 app.Run();
