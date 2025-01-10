9c058835678f04405e3495275df157c905e41c55
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-10 12:47:54 -0300

    create migration customer

---
+++ secao_10/aula_01/MicroservicoApi/Customer/Migrations/20250110154407_InitialCreated.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Customer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    customer_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customers", x => x.customer_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customers");
+        }
+    }
+}
