944e2c432777d62b405cad68ca8b601bf2ac5c6a
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-10 18:33:30 -0300

    add inject services and  repositories

--- secao_10/aula_01/ApiGatewayAuth/CatalogService/Program.cs
+++ secao_10/aula_01/ApiGatewayAuth/CatalogService/Program.cs
@@ -1,15 +1,20 @@
 using CatalogService.Data;
+using CatalogService.Filters;
+using CatalogService.Repositories;
+using CatalogService.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<CustomExceptionsFilter>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
