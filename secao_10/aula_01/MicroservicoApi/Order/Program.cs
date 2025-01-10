b6e3ee2a956b3619033d78762e3a5f1f409f8773
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-10 14:14:38 -0300

    cofigure program.cs

--- secao_10/aula_01/MicroservicoApi/Order/Program.cs
+++ secao_10/aula_01/MicroservicoApi/Order/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Order.Data;
+using Order.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDBContext>(option => option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 var app = builder.Build();
 
