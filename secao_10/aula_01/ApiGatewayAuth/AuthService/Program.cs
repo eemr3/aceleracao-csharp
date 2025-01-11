f44ee03086609a797e928332c15d6701b51fea97
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-11 11:38:35 -0300

    add services for repositories and services

--- secao_10/aula_01/ApiGatewayAuth/AuthService/Program.cs
+++ secao_10/aula_01/ApiGatewayAuth/AuthService/Program.cs
@@ -1,15 +1,22 @@
 using AuthService.Data;
+using AuthService.Filters;
+using AuthService.Repositories;
+using AuthService.Services.Auth;
+using AuthService.Services.UserService;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add<CustomExceptionsFilter>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IAuth, Auth>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
