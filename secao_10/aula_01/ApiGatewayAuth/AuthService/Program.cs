0dd43e4c3b7552f21a80d81e105a187a7e74168a
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-12 11:14:30 -0300

    create initial migration for AuthService

--- secao_10/aula_01/ApiGatewayAuth/AuthService/Program.cs
+++ secao_10/aula_01/ApiGatewayAuth/AuthService/Program.cs
@@ -1,3 +1,8 @@
+using AuthService.Data;
+using AuthService.Repositories;
+using AuthService.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
