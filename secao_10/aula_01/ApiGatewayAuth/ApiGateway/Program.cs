8c80988715ad333b0af877df208d3d5a72569652
Emerson Moreira <eemr3@yahoo.com.br>
2025-01-12 12:15:05 -0300

    create gateway api

--- secao_10/aula_01/ApiGatewayAuth/ApiGateway/Program.cs
+++ secao_10/aula_01/ApiGatewayAuth/ApiGateway/Program.cs
@@ -1,6 +1,32 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(options =>
+    {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+      };
+    }
+  );
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
 
 app.Run();
